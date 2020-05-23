e7984eaaf5eb2c0a71707515925d61e2b571f5ec
YooJini <wlsdl6685@gmail.com>
2020-05-23 20:02:04 +0900

    5. 메쉬 렌더러
    - 유니티버전: Unity 2019.3.13f1(64-bit)

--- Unity_Tutorial/Assets/Scripts/Test.cs
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -4,51 +4,28 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]
+    private Material red_Mat;
+    [SerializeField]
+    private Material green_Mat;
 
-    private BoxCollider col;
-  
-
+    private MeshRenderer mesh;
+    // Start is called before the first frame update
     void Start()
     {
-        col = GetComponent<BoxCollider>();
+        mesh = GetComponent<MeshRenderer>();
     }
 
-    // void Update()
-    // {
-    //    if(Input.GetKeyDown(KeyCode.W))
-    //     {
-    //        //속성
-    //        // Debug.Log("col.bounds"+col.bounds);
-    //        // Debug.Log("col.bounds.extents"+col.bounds.extents);
-    //        // Debug.Log("col.bounds.extents.x"+col.bounds.extents.x);
-    //        // Debug.Log("col.size"+col.size);
-    //        // Debug.Log("col.center"+col.center);
-    //
-    //        //메소드
-    //        //레이저 쏘기, 감지
-    //        if(Input.GetMouseButtonDown(0))
-    //         {
-    //             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //             RaycastHit hitInfo;
-    //             if(col.Raycast(ray, out hitInfo, 1000))
-    //             {
-    //                 this.transform.position = hitInfo.point;
-    //             }
-    //         }
-    //     }
-    // }
-  
-   // private void OnTriggerEnter(Collider other)
-   // {
-   //     
-   // }
-    private void OnTriggerExit(Collider other)
+    // Update is called once per frame
+    void Update()
     {
-        other.transform.position = new Vector3(0,2,0);
-    }
-   //머물고 있는 동안 실행
-    private void OnTriggerStay(Collider other)
-    {
-        other.transform.position += new Vector3(0, 0, 0.01f);
+        if(Input.GetMouseButton(0))
+        {
+            mesh.material = green_Mat;
+        }
+        else
+        {
+            mesh.material = red_Mat;
+        }
     }
 }
