e07399343f893b9b2279c35668c8233ba12ec7d6
YooJini <wlsdl6685@gmail.com>
2020-05-24 22:13:27 +0900

    23. 드래그&드롭
    - 유니티버전: Unity 2019.3.13f1(64-bit)

--- Unity_Tutorial/Assets/Scripts/Test.cs
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -2,62 +2,31 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 
-public class Test : MonoBehaviour
+
+public class Test : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    
-    NavMeshAgent agent;
-
-    [SerializeField] private Transform[] tf_Destination;
-
-    private Vector3[] wayPoints;
-    
-
-    private void Start()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-      
-        agent = GetComponent<NavMeshAgent>();
-        
-    }
-    private void Update()
-    {
-       Collider[] col = Physics.OverlapSphere(transform.position, 10f);
-
-        if (col.Length>0)
-        {
-            for (int i = 0; i < col.Length; i++)
-            {
-                Transform tf_Target = col[i].transform;
-
-                if(tf_Target.name=="플레이어")
-                {
-                    NavMeshPath path = new NavMeshPath();
-                    agent.CalculatePath(tf_Target.position, path);
-
-                    Vector3[] wayPoints = new Vector3[path.corners.Length + 2];
-                    wayPoints[0] = transform.position;
-                    wayPoints[wayPoints.Length - 1] = tf_Target.position;
-
-                    float _distance = 0f;
-                    for (int p = 0; p < path.corners.Length; p++)
-                    {
-                        wayPoints[p + 1] = path.corners[p];
-                        _distance = Vector3.Distance(wayPoints[p], wayPoints[p + 1]);
-                    }
-
-                    if(_distance<=10f)
-                    {
-                        agent.SetDestination(tf_Target.position);
-                    }
-                }
-            }
-        }
+        Debug.Log("Begin Drag");
     }
 
-    
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
 }
    
    
