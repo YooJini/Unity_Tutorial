fdf61a264d8d987b6eeb32eefc44e1c69abfd61f
YooJini <wlsdl6685@gmail.com>
2020-05-24 21:34:58 +0900

    20. 네비게이션
    - 유니티버전: Unity 2019.3.13f1(64-bit)

--- Unity_Tutorial/Assets/Scripts/Test.cs
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -3,45 +3,54 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 
 public class Test : MonoBehaviour
 {
+    Rigidbody myRigid;
+    [SerializeField] private float moveSpeed;
 
-    private float createTime = 1f;
-    private float currentCreateTime;
+    NavMeshAgent agent;
 
-    [SerializeField] private GameObject go_BulletPrefab;
+    [SerializeField] private Transform[] tf_Destination;
 
+    private Vector3[] wayPoints;
+    
+
+    private void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+
+        wayPoints = new Vector3[tf_Destination.Length + 1];
+
+        for (int i = 0; i <tf_Destination.Length; i++)
+        
+            wayPoints[i] = tf_Destination[i].position;
+         wayPoints[wayPoints.Length - 1] = transform.position;
+        
+    }
     private void Update()
     {
-        
-        Collider[] col = Physics.OverlapSphere(transform.position, 5f);
-
-        if(col.Length>0)
-        {
-            for (int i = 0; i < col.Length; i++)
-            {
-                Transform tf_Target = col[i].transform;
-
-                if(tf_Target.tag=="Player")
-                {
-                    
-                    Quaternion rotation = Quaternion.LookRotation(tf_Target.position - this.transform.position);
-                    transform.rotation = rotation;
-
-                    currentCreateTime += Time.deltaTime;
-                    if (currentCreateTime >= createTime)
-                    {
-                       GameObject _tmp = Instantiate(go_BulletPrefab, transform.position, rotation);
-                        Physics.IgnoreCollision(_tmp.GetComponent<Collider>(), tf_Target.GetComponent<Collider>());
-                        currentCreateTime = 0;
-                    }
-                }
-            }
-        }
+        Patrol();
+       // if(Input.GetKey(KeyCode.W))
+       // {
+       //     agent.SetDestination(tf_Destination.position);
+       // }
+      
     }
 
+    void Patrol()
+    {
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, wayPoints[i]) <= 0.1f)
+                agent.SetDestination(wayPoints[i + 1]);
+            else
+                agent.SetDestination(wayPoints[0]);
+        }
+    }
 }
    
    
