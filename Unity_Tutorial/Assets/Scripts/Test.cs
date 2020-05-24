339ac8b045b6045b8e5cd55ef5d09e00a06df384
YooJini <wlsdl6685@gmail.com>
2020-05-24 17:41:00 +0900

    16. 오디오 소스
    - 유니티버전: Unity 2019.3.13f1(64-bit)

--- Unity_Tutorial/Assets/Scripts/Test.cs
+++ Unity_Tutorial/Assets/Scripts/Test.cs
@@ -4,61 +4,27 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
-public class Dialog
-{
-    [TextArea]
-    public string dialog;
-    public Sprite cg;
 
-}
 public class Test : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer sprite_StandingCG;
-    [SerializeField] private SpriteRenderer sprite_DialogBox;
-    [SerializeField] private Text txt_Dialog;
+    [SerializeField] AudioSource theAudio;
 
-    private bool isDialog = false;
+    [SerializeField] private AudioClip[] clip;
 
-    private int count = 0;
 
-    [SerializeField] private Dialog[] dialog;
-
-    private void OnOff(bool _flag)
+    private void Start()
     {
-        sprite_DialogBox.gameObject.SetActive(_flag);
-        sprite_StandingCG.gameObject.SetActive(_flag);
-        txt_Dialog.gameObject.SetActive(_flag);
-        isDialog = _flag;
+        theAudio = GetComponent<AudioSource>();
     }
 
-    public void ShowDialog()
+    public void PlaySE()
     {
-        OnOff(true);
-        count = 0;
-        NextDialog();
+        int _temp = Random.Range(0, 4);
+
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
     }
 
-    private void NextDialog()
-    {
-        txt_Dialog.text = dialog[count].dialog;
-        sprite_StandingCG.sprite = dialog[count].cg;
-        count++;
-    }
-
-    
-
-    private void Update()
-    {
-        if(isDialog)
-        {
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                if (count < dialog.Length)
-                    NextDialog();
-                else
-                    OnOff(false);
-            }
-        }
-    }
 }
+   
+   
